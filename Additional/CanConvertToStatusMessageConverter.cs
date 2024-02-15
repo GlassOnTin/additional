3aebe8bf6fdfdc4b2e370244566347e218dc6174
Ian Williams <ianross.williams@lr.org>
2024-02-15 15:35:30 +0000

    Implemented the MainViewModel class to handle input changed and calculate events.  Use the RelayCommand pattern.

---
+++ Additional/CanConvertToStatusMessageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Additional
+{
+    public class CanConvertToStatusMessageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool canCalculate)
+            {
+                return canCalculate ? "Ready to calculate" : "Enter both numbers to calculate";
+            }
+            return "Invalid input";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
